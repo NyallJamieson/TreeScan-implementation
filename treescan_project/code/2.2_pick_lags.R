5e33bd8b021f6446a796218eb6056da158a44172
NyallJamieson <137408584+NyallJamieson@users.noreply.github.com>
2026-04-10 09:04:14 -0500

    Update 2.2_pick_lags.R

--- treescan_project/code/2.2_pick_lags.R
+++ treescan_project/code/2.2_pick_lags.R
@@ -1,37 +1,95 @@
 # Is it a new month? (we assess lag in new month)
 # Set to true if you want to change (or re-assess) the lags you're using
-initial_lags <- c(1,4)
+initial_lags <- c(1, 4)
 
-# Location of this run_full_pipeline.R script
-script_path <- paste0(parent_dir, "/code/2.2_pick_lags.R")
+script_path <- file.path(parent_dir, "code", "2.2_pick_lags.R")
 
-# Load required libraries
-library("png")
-library("svDialogs")
+library(png)
+library(tcltk)
+library(timeDate)
 
-if (isTRUE(new_month) || isTRUE(first_time)){
-  img <- readPNG(paste0(parent_dir, "/lag/plots/lag_curve_", year_month, ".png"))
-  plot.new()
-  rasterImage(img, 0, 0, 1, 1)
+get_lags_with_timeout <- function(timeout = 30, default = initial_lags) {
+  tt <- tktoplevel()
+  tkwm.title(tt, "Lag Selection")
   
-  # If we change the lag selection then we need to update this in the script
-  line_x <- 3
+  label <- tklabel(
+    tt,
+    text = paste(
+      "Look at your lag plot.\n",
+      "Enter new lags separated by commas,\n",
+      "or wait", timeout, "seconds to keep existing lags:",
+      paste(default, collapse = ", ")
+    )
+  )
+  tkpack(label, padx = 10, pady = 10)
   
-  # Ask for values
-  input <- dlgInput("Look at your lag plot. Do you want to consider new lags? If so, then type below (separated by commas). Type 1 to mean you want to use data up until a day ago, etc.")$res
+  entry <- tkentry(tt, width = 20)
+  tkpack(entry, padx = 10, pady = 5)
   
-  # Build replacement line
-  new_line <- paste0("initial_lags <- c(", input, ")")
+  result <- default
   
-  # Read script
-  lines <- readLines(script_path)
+  on_ok <- function() {
+    val <- trimws(tclvalue(tkget(entry)))
+    if (nzchar(val)) {
+      parsed <- suppressWarnings(as.numeric(trimws(strsplit(val, ",")[[1]])))
+      if (!any(is.na(parsed))) {
+        result <<- parsed
+      } else {
+        message("Invalid input. Keeping existing lags: ",
+                paste(default, collapse = ", "))
+      }
+    }
+    if (as.logical(tkwinfo("exists", tt))) tkdestroy(tt)
+  }
   
-  # Replace target line
-  lines[line_x] <- new_line
+  ok_button <- tkbutton(tt, text = "OK", command = on_ok)
+  tkpack(ok_button, pady = 10)
   
-  # Save script
-  writeLines(lines, script_path)
+  tcl("after", as.integer(timeout * 1000), function() {
+    if (as.logical(tkwinfo("exists", tt))) {
+      message("No input received. Keeping existing lags: ",
+              paste(default, collapse = ", "))
+      tkdestroy(tt)
+    }
+  })
   
-  eval(parse(text = new_line))
+  tkwait.window(tt)
+  result
+}
+
+is_weekend_or_us_holiday <- function(date = Sys.Date()) {
+  date <- as.Date(date)
   
+  # Weekend?
+  is_weekend <- weekdays(date) %in% c("Saturday", "Sunday")
+  
+  # US federal holidays for the relevant year
+  hols <- as.Date(holidayNYSE(as.numeric(format(date, "%Y"))))
+  is_holiday <- date %in% hols
+  
+  is_weekend || is_holiday
+}
+
+if (isTRUE(new_month) || isTRUE(first_time)) {
+  if (is_weekend_or_us_holiday()) {
+    message("Today is a weekend or US holiday. Keeping existing lags: ",
+            paste(initial_lags, collapse = ", "))
+  } else {
+    img <- readPNG(file.path(parent_dir, "lag", "plots",
+                             paste0("lag_curve_", year_month, ".png")))
+    plot.new()
+    rasterImage(img, 0, 0, 1, 1)
+    
+    line_x <- 1
+    
+    new_lags <- get_lags_with_timeout(timeout = 30, default = initial_lags)
+    
+    new_line <- paste0("initial_lags <- c(", paste(new_lags, collapse = ","), ")")
+    
+    lines <- readLines(script_path)
+    lines[line_x] <- new_line
+    writeLines(lines, script_path)
+    
+    initial_lags <- new_lags
+  }
 }
