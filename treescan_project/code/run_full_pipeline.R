4cf4af4cbc895b33968d3f87b0e969dcd8e028ce
NyallJamieson <137408584+NyallJamieson@users.noreply.github.com>
2026-04-23 20:59:20 -0500

    Update run_full_pipeline.R

--- treescan_project/code/run_full_pipeline.R
+++ treescan_project/code/run_full_pipeline.R
@@ -104,7 +104,7 @@ if (lag_choice == 1){
     lines <- readLines(script_path)
     
     # Replace target line
-    lines[114] <- new_line
+    lines[115] <- new_line
     
     # Save script
     writeLines(lines, script_path)
