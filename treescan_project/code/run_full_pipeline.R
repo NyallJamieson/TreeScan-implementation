2ee728dacec37ebd781c90bdb1ad3aa931f93e0f
NyallJamieson <nyalljamieson2000@outlook.com>
2026-05-07 08:27:30 -0500

    we need to change the line that saves when runnning 2.3 to update your initial_lags. The line got pushed down when we added new code above

--- treescan_project/code/run_full_pipeline.R
+++ treescan_project/code/run_full_pipeline.R
@@ -108,7 +108,7 @@ if (lag_choice == 1){
     lines <- readLines(script_path)
     
     # Replace target line
-    lines[115] <- new_line
+    lines[119] <- new_line
     
     # Save script
     writeLines(lines, script_path)
